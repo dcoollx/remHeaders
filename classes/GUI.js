495a7365eafbe0929c7fe3cd60d840e8c4013932
David Queen <dsqjr@msn.com>
2020-11-16 11:05:19 -0500

    v1.2

--- classes/GUI.js
+++ classes/GUI.js
@@ -1,22 +1,38 @@
 /* eslint-disable no-console */
+import Writer from './Writer';
+
 export default class GUI{
-  constructor(writer,list){
+  constructor(list){
     this.output = document.createElement('textarea');
     this.output.style = 'color:darkblue; background-color:darkblue;border:none;float:right';
     this.mainList = list;
     this.currentNode = list.head;
-    this.writer = writer;
+    this.writer = new Writer();
     this.writer.setOutput(this.output);
     this.container = document.createElement('center');
     this.container.id = 'ae_header_rem';
     this.style = document.createElement('style');
+    this.input = document.createElement('input');
+    this.input.id = 'ae_headerRem_input';
+    this.container.append(this.input);
     this.style.innerHTML = `
-
-    #headerText, #ae_header_rem label, #ae_header_rem code{
-      color:white
+    *:focus{
+      border: dotted red 5px;
     }
+    #headerText, #ae_header_rem label, #ae_header_rem code{
+      color:white;
+    }
+    #ae_header_rem label{
+      grid-row: 1/span 2
+    }
+
       #ae_header_rem {
-        position: fixed !important;
+      overflow:scroll;
+      padding:15px;
+      display:grid;
+      grid-template-rows: auto auto auto;
+      max-height: 25vh;
+      position: fixed !important;
       color: white;
       background-color: darkblue;
       border : 2px solid black;
@@ -44,6 +60,10 @@ export default class GUI{
         color: black;
         background-color:white;
       }
+      #secondary > *{
+        max-height:5%;
+        max-width:5%;
+      }
       [ae_headers_autorem]{
         background-color : bisque;
         border: 5px solid red;
@@ -51,75 +71,71 @@ export default class GUI{
     `;
     document.body.append(this.style);
     this.container.innerHTML = `
-      <h2 class="ae_header_exclude">{<strong id="headerText"></strong> }| <label for="headerLevel">current level: </label><code id="headerLevel"></code></h2>
+      <h2 class="ae_header_exclude">"<strong id="headerText"></strong>" <span id="secondary"></span></h2>
     `;
+    //////////////////
     let label = document.createElement('label');
     label.setAttribute('for','ae_headerRem_input');
-    label.innerText = 'set new heading level';
-    let input = document.createElement('input');
-    let suggestion = document.createElement('code');
-    let suggestion_label = document.createElement('label');
-    suggestion.innerHTML = '?';
-    suggestion_label.innerHTML = 'suggested level';
-    suggestion_label.htmlFor = 'suggestion';
-    suggestion.id = 'suggestion';
-    input.id = 'ae_headerRem_input';
-    input.type = 'number';
-    input.min = 0;
-    input.max = 6;
-    input.title = 'set to 0 for presentation';
-    this.container.append(suggestion_label);
-    this.container.append(suggestion);
-    this.container.append(document.createElement('br'));
+    label.innerText = 'change the ' + this.currentNode.attr;
     this.container.append(label);
-    this.container.append(input);
-    this.container.append(document.createElement('br'));
+    this.container.append(this.input); 
+    /////////
+    let controls = document.createElement('div');
+    controls.id = 'controls';
     this.controls_prev = document.createElement('button');
     this.controls_prev.innerText = '<< prev';
     this.controls_next = document.createElement('button');
     this.controls_next.innerText = 'next >>';
     this.nextButton = ()=>{
-      //console.log('original', this.currentNode.originalLvl,'input',input.value, 'node level',this.currentNode.level);
-      if(Number(input.value) !== this.currentNode.level){
-        this.currentNode.setLevel(input.value);
-        //todo make this run on finish
-        this.writer.writeLn(this.currentNode);
-      }
       if(this.currentNode.next){
+        //inspect(this.currentNode.next);
         this.currentNode = this.currentNode.next; 
         this.render();
       }else{
         this.writer.display();
         this.cleanUp();
       }
-    }; 
+    };
+    this.input.addEventListener('change', (e)=>{
+      this.currentNode.changeValue(e.target.value);
+
+    }); 
     this.controls_next.addEventListener('click',this.nextButton);
     this.controls_prev.addEventListener('click',(e)=>{
       this.currentNode = this.currentNode.prev;
       this.render();
     });
-    this.container.append(this.controls_prev);
-    this.container.append(this.controls_next);
+    controls.append(this.controls_prev);
+    controls.append(this.controls_next);
     //
     let finished = document.createElement('button');
     finished.innerText = 'finished';
     finished.addEventListener('click',(e)=>{
+
+      this.mainList.traverse((n)=>{
+        if(n.valueChanged){
+          this.writer.writeLn(n);
+        }
+      });
       this.writer.display();
       this.cleanUp();
       //delete this;
     });
-    this.container.append(finished);
+    controls.append(finished);
+    this.container.append(controls);
     //extra controls
 
     let showLL = document.createElement('button');
     showLL.innerText = 'show header order';
     showLL.addEventListener('click',()=>this.mainList._display());
-    this.container.append(showLL);
+    //this.container.append(showLL);
     let showDepth = document.createElement('button');
-    showDepth.innerText = 'show header depth';
+    showDepth.innerText = 'show element depth';
     showDepth.addEventListener('click',()=>this.mainList._displayDepth());
-    this.container.append(showDepth);
+    //this.container.append(showDepth);
     this.container.append(this.output);
+    document.body.append(this.container);
+    /////////////////////////////////
     this.render();
   }
 
@@ -132,16 +148,21 @@ export default class GUI{
     if(window.$ae){
       window.AudioEye.focusElement(this.currentNode.element);
     }
-    let input = this.container.querySelector('#ae_headerRem_input');
-    input.value = this.currentNode.level;
+      
+    this.input.type = this.currentNode.inputType;
+    this.input.value = this.currentNode.value;
+    //this.input.min = 0;
+    //this.input.max = 6;
+    //this.input.title = 'set to 0 for presentation';
+    
     if(this.currentNode){
-      this.container.querySelector('#suggestion').innerText = '( ' + this.currentNode.suggestedLvl + ' )';
-      this.container.querySelector('#headerText').innerText = this.currentNode.element.innerText.trim() !== '' ? this.currentNode.element.innerText.trim() : this.currentNode.element.querySelector('img[alt]').getAttribute('alt');//TODO, add grabing of img alts
-      this.container.querySelector('#headerLevel').innerText = this.currentNode.level;
-      document.body.append(this.container);
+      this.container.querySelector('#headerText').innerText = this.currentNode.element.innerText.trim() || this.currentNode.value;
+      this.container.querySelector('#secondary').innerHTML = this.currentNode.display;
+       
     }else{
       alert('end of list');
       this.controls_next.setAttribute('disabled','');
     }
+    
   }
 }
--- index.js
+++ index.js
@@ -1,125 +1,24 @@
-import LL from './classes/linklist';
+import LL, { HNode } from './classes/linklist';
 import regions from './classes/Landmarks';
 import Writer from './classes/Writer';
 import GUI from './classes/GUI';
+import Wizard from './classes/wizard';
+import {ImgNode} from './classes/linklist';
 
 
+window.remHeaders = function(args = {verbose : true, suggestions: true, forcedHeader: null, analyzeOnly : false, ignore : null,  notHeaders : null, mainHeaderConfirmed : false}){//currently only take selector strings
+  let {verbose, suggestions, analyzeOnly, notHeaders, mainHeaderConfirmed, forcedHeader, ignore} = args;
 
-window.remHeaders = function(verbose = false, forcedHeader=null, analyzeOnly = true, ignore=null,  notHeaders = null, mainHeaderConfirmed = false,){//currently only take selector strings
-  console.log('starting header rem');
-  if(analyzeOnly){
-    //updateAll = (n)=>null;
-    verbose ? console.log('analyzing only... adding helpers') : null;
-  }
-  let landmarks = new regions();
-
-  ////now will rem each landmark
-  let mainList = new LL();
-  mainList.fromArray(document.querySelectorAll('h1,h2,h3,h4,h5,h6,[role="heading"]'));
-  if(landmarks.allRegions){
-    for(let region of ['banner','main', 'footer']){
-      console.log(region);
-      landmarks.findContaining(region, mainList);
-      //check for regions
+  let log = function(...msg){
+    if(verbose){
+      console.log(...msg);
     }
-    verbose ? console.log(landmarks) : null;
-  }
- 
-  // if(analyzeOnly)
-  //return list;
-  verbose ? console.log(mainList._displayDepth()): null;
-  //////////version 3
-  let output = new Writer();
-  ///////end version 3
-  /////////////new stuff, by landmark
-  //header landmark
-  if(landmarks.allRegions){
-    let high = 6;
-    landmarks.banner.list.traverse((n)=>{
-      if(n.level < high)
-        high = n.level;
-      if(n === landmarks.banner.list.tail)//todo add this check to traverse
-        return true;
-    });
-    verbose ? console.log('banner lowest level: ', high) : null;
-    landmarks.banner.list.traverse((n)=>{
-      if(high !== 2){//dont change if correct
-        if(n.level === high){
-          n.setSuggestedLevel(2); 
-        }else{
-          let diff = high - 2;
-          verbose ? console.log('node diff = ', diff) : null;
-          n.setSuggestedLevel(n.level - diff);//move down by the same 
-        }
-      }
-      if(n === landmarks.banner.list.tail)
-        return true;
-    });
-  }//end for now
-  //landmarks.banner.list.traverse(groupFix);
-  ///////////find main header
-  mainList.mainHeader =  mainList.traverse(n=>n.level===1);//create sub ll
-  //console.log('main list', list);
-  if(mainList.mainHeader === null){
-  //there is no main header
-    let logoHeader = document.querySelector('header a[href="/"] > img:eq(0)');//main logo
-    if(!logoHeader){
-      this.console.warn('no main header found please add one to page or designate one in function');
-    }
-    this.console.warn('no h1 found, changing logo to h1');
-    logoHeader.setAttribute('role','heading');
-    logoHeader.setAttribute('aria-level','1');//todo, this doesnt trigger adding to the list of rems
-    //todo wrap in function to be invokeed here
-  }
-  mainList.mainHeader.element.setAttribute('ae_headers_rem_main','');
-  let n = mainList.mainHeader.next;
-  while(n.next){
-    if(n.level ===1){
-      n.setSuggestedLevel(2);
-    } 
-    n = n.next;//increment */
-  } //
-  //main
-
-  //landmarks.main.list.traverse(oneStepRule);
-
-  //footer
-  let high = 6;
-  landmarks.footer.list.traverse((n)=>{
-    if(n.level < high)
-      high = n.level;
-    if(n === landmarks.banner.list.tail)//todo add this check to traverse
-      return true;
-  });
-
-  landmarks.footer.list.traverse((n)=>{
-    if(high !== 2){//dont change if correct
-      if(n.level === high){
-        n.setSuggestedLevel(2);
-      }else{
-        n.setSuggestedLevel(0);//role presentation
-      }
-    }
-    if(n === landmarks.banner.list.tail)
-      return true;
-  });
+  };
+  new Wizard(HNode, 'h1,h2,h3,h4,h5,h6, [role="heading][aria-level]');
+};
 
 
-  //////////
-  let subList = new LL();
-  if(mainList.mainHeader.prev !== null){//if main is not top of list
-    subList.head = mainList.mainHeader;//todo applie one step rules for sublist, for list aplly specialized checks
-    subList.tail = mainList.tail;
-  //set any lvl 1 other than main to lvl 2
-  }else{
-    //there is no sublist
-    subList = mainList;
-  }
-  console.log('sublist',subList._display());
 
-  //subList.traverse(n=>oneStepRule(n));//aply one ste rule to sub list
-  mainList.mainHeader.setSuggestedLevel(1);
-  //mainList.traverse();
-  let panel = new GUI(output,mainList);
-  return mainList;
+window.remAlts = function(){
+  new Wizard(ImgNode, 'img');
 };
